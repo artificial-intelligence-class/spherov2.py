591eeeed1b09d2726910e025fa323707185e6760
Richard Beattie <richard.beattie91@gmail.com>
2023-06-12 16:03:50 -0400

    Update tcp_adapter

--- spherov2/adapter/tcp_adapter.py
+++ spherov2/adapter/tcp_adapter.py
@@ -1,7 +1,5 @@
-import socket
 import struct
-import threading
-from concurrent import futures
+import asyncio
 from typing import NamedTuple
 
 from spherov2.adapter.tcp_consts import RequestOp, ResponseOp
@@ -28,124 +26,124 @@ def get_tcp_adapter(host: str, port: int = 50004):
 
     class TCPAdapter:
         @staticmethod
-        def scan_toys(timeout=5.0):
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.connect((host, port))
+        async def scan_toys(timeout=5.0):
+            reader, writer = await asyncio.open_connection(host, port)
             try:
-                s.sendall(RequestOp.SCAN + struct.pack('!f', timeout))
-                code = recvall(s, 1)
+                writer.write(RequestOp.SCAN + struct.pack('!f', timeout))
+                await writer.drain()
+                code = await reader.readexactly(1)
                 if code == ResponseOp.ERROR:
-                    size = to_int(recvall(s, 2))
-                    data = recvall(s, size)
+                    data = (await reader.readuntil(b'\0'))[:-1]
                     raise Exception(data.decode('utf_8'))
                 elif code != ResponseOp.OK:
                     raise SystemError(f'Unexpected response op code {code}')
-                num_devices = to_int(recvall(s, 2))
+                num_devices = to_int(await reader.readexactly(2))
                 devices = []
                 for _ in range(num_devices):
-                    name_size = to_int(recvall(s, 2))
-                    name = recvall(s, name_size).decode('utf_8')
-                    address_size = to_int(recvall(s, 2))
-                    addr = recvall(s, address_size).decode('ascii')
-                    devices.append(MockDevice(name, addr))
+                    name = (await reader.readuntil(b'\0'))[:-1].decode('utf_8')
+                    address = (await reader.readuntil(b'\0'))[:-1].decode('ascii')
+                    devices.append(MockDevice(name, address))
                 return devices
             finally:
-                s.sendall(RequestOp.END)
-                s.close()
+                writer.write(RequestOp.END)
+                await writer.drain()
+                writer.close()
+                await writer.wait_closed()
 
         @staticmethod
-        def scan_toy(name: str, timeout: float = 5.0):
+        async def scan_toy(name: str, timeout: float = 5.0):
             name = name.encode('utf_8')
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.connect((host, port))
+            reader, writer = await asyncio.open_connection(host, port)
             try:
-                s.sendall(RequestOp.FIND + to_bytes(len(name), 2) +
+                writer.write(RequestOp.FIND + to_bytes(len(name), 2) +
                           name + struct.pack('!f', timeout))
-                code = recvall(s, 1)
+                await writer.drain()
+                code = await reader.readexactly(1)
                 if code == ResponseOp.ERROR:
-                    size = to_int(recvall(s, 2))
-                    if size == 0:
-                        return None
-                    data = recvall(s, size)
+                    data = (await reader.readuntil(b'\0'))[:-1]
                     raise Exception(data.decode('utf_8'))
                 elif code != ResponseOp.OK:
                     raise SystemError(f'Unexpected response op code {code}')
-                name_size = to_int(recvall(s, 2))
-                name = recvall(s, name_size).decode('utf_8')
-                address_size = to_int(recvall(s, 2))
-                addr = recvall(s, address_size).decode('ascii')
-                return MockDevice(name, addr)
+                name = (await reader.readuntil(b'\0'))[:-1].decode('utf_8')
+                address = (await reader.readuntil(b'\0'))[:-1].decode('ascii')
+                return MockDevice(name, address)
             finally:
-                s.sendall(RequestOp.END)
-                s.close()
+                writer.write(RequestOp.END)
+                await writer.drain()
+                writer.close()
+                await writer.wait_closed()
 
         def __init__(self, address):
-            self.__socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            self.__socket.connect((host, port))
             address = address.encode('ascii')
+
+            self.__reader = None
+            self.__writer = None
 
             self.__sequence = 0
             self.__sequence_wait = {}
 
             self.__callbacks = {}
-            self.__thread = threading.Thread(target=self.__recv)
-            self.__thread.start()
+
+        async def connect(self):
+            self.__reader, self.__writer = await asyncio.open_connection(host, port)
+            asyncio.ensure_future(self.__recv())
             try:
-                self.__send(RequestOp.INIT, to_bytes(
-                    len(address), 2) + address)
+                await self.__send(RequestOp.INIT, self.__address.encode('ascii') + b'\0')
             except:
-                self.close()
+                await self.disconnect()
                 raise
 
-        def __recv(self):
+        async def __recv(self):
             while True:
                 try:
-                    code = recvall(self.__socket, 1)
+                    code = await self.__reader.readexactly(1)
                 except:
                     break
                 if code == ResponseOp.OK:
-                    self.__sequence_wait.pop(
-                        recvall(self.__socket, 1)[0]).set_result(None)
+                    self.__sequence_wait.pop((await self.__reader.readexactly(1))[0]).set_result(None)
                     continue
-                size = to_int(recvall(self.__socket, 2))
-                data = recvall(self.__socket, size)
+                data = (await self.__reader.readuntil(b'\0'))[:-1]
                 if code == ResponseOp.ON_DATA:
                     uuid = data.decode('ascii').lower()
-                    size = recvall(self.__socket, 1)[0]
-                    data = recvall(self.__socket, size)
+                    size = (await self.__reader.readexactly(1))[0]
+                    data = await self.__reader.readexactly(size)
                     for f in self.__callbacks.get(uuid, []):
                         f(uuid, data)
                 elif code == ResponseOp.ERROR:
                     err = Exception(data.decode('utf_8'))
-                    self.__sequence_wait.pop(recvall(self.__socket, 1)[
-                                             0]).set_exception(err)
+                    self.__sequence_wait.pop((await self.__reader.readexactly(1))[0]).set_exception(err)
 
-        def __send(self, cmd, payload):
+        async def __send(self, cmd, payload):
             if not self.__thread.is_alive():
                 raise ConnectionError('Connection is lost')
             seq = self.__sequence
             self.__sequence = (self.__sequence + 1) % 0x100
-            f = self.__sequence_wait[seq] = futures.Future()
-            self.__socket.sendall(cmd + bytes([seq]) + payload)
-            f.result()
+            f = self.__sequence_wait[seq] = asyncio.futures.Future()
+            self.__writer.write(cmd + bytes([seq]) + payload)
+            await self.__writer.drain()
+            await f
 
-        def close(self):
-            self.__socket.sendall(RequestOp.END)
-            self.__socket.close()
-            self.__thread.join()
+        async def close(self):
+            # self.__socket.sendall(RequestOp.END)
+            # self.__socket.close()
+            # self.__thread.join()
+            self.__writer.write(RequestOp.END)
+            await self.__writer.drain()
+            self.__writer.close()
+            await self.__writer.wait_closed()
 
-        def set_callback(self, uuid, cb):
+        async def set_callback(self, uuid, cb):
             if uuid in self.__callbacks:
                 self.__callbacks[uuid].add(cb)
             else:
                 self.__callbacks[uuid] = {cb}
                 buf = uuid.encode('ascii')
-                self.__send(RequestOp.SET_CALLBACK,
+                await self.__send(RequestOp.SET_CALLBACK,
                             to_bytes(len(buf), 2) + buf)
 
-        def write(self, uuid, data):
+        async def write(self, uuid, data):
             uuid = uuid.encode('ascii')
-            self.__send(RequestOp.WRITE, to_bytes(len(uuid), 2) +
+            await self.__send(RequestOp.WRITE, to_bytes(len(uuid), 2) +
                         uuid + to_bytes(len(data), 2) + data)
 
     return TCPAdapter
